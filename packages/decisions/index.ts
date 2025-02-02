629e70270706efd45be104c497b567a36a0b22ff
LouiFi <louis@popoyoko.fr>
2025-02-02 16:33:13 +0100

    ⚰️

--- packages/decisions/index.ts
+++ packages/decisions/index.ts
@@ -1,3 +1,1 @@
-import type { Organisation, Brand, Decision } from "./types/index";
-
-export type { Organisation, Brand, Decision };
+export type { Brand, Decision, Organisation } from "./types/index";
